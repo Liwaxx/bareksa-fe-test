f73928967061443bba1cc9778047365d21c12aec
liwax <liwaalex99@gmail.com>
2021-10-06 10:55:24 +0700

    arrange react folder

---
+++ src/constants/styles.js
@@ -0,0 +1,13 @@
+const styles = {
+  colors : {
+    PRIMARY_500 : '#82C341',
+    PRIMARY_700 : '#5F9F2F',
+    BLACK_COOL : '#E4EAEB',
+    VIOLET : '#725E9C',
+    BLUE : '#5C8F94',
+    ORANGE : '#EBA45E',
+    GREEN : '#789764'
+  }
+}
+
+export default styles;
